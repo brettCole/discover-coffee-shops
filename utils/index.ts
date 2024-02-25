5b00fa43c6e78e01272ed2ed70ee72d50167c01a
brett.cole <acodercalledcole@gmail.com>
2024-02-25 12:19:13 -0500

    production domain

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,6 @@
 export const getDomain = () => {
   return new URL(process.env.NODE_ENV === 'production'
-    // ? 'https://discover-coffee-stores-lates.vercel.app'
-    ? 'http://localhost:3000'
+    ? 'https://discover-coffee-shops-one.vercel.app/'
     : 'http://localhost:3000'
   )
 }
